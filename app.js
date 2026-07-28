1d753f0063a769c76533c27c74eccc82b2873b3f
Ferrah Nessrine <nessrineferrah01@hotmail.com>
2026-07-28 14:57:14 +0200

    Ajout de dotenv, sécurisation de l’URI MongoDB et documentation backend. Création du fichier .env contenant l’URI MongoDB (MONGO_URI=...) : ajout du fichier .env dans .gitignore pour empêcher son suivi par Git. Intégration de dotenv dans app.js : import dotenv from dotenv, dotenv.config() pour charger les variables d’environnement et remplacement de l’URI MongoDB en clair par process.env.MONGO_URI. Ajout d’un README dans le dossier backend expliquant : comment installer les dépendances, comment configurer le fichier .env, comment lancer le backend avec nodemon et les technologies utilisées.

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
-
+// En ES Modules, __filename et __dirname n'existent plus nativement.
 import bookRoutes from "./routes/book.js";
 import userRoutes from "./routes/user.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+// On les recrée manuellement à partir de import.meta.url pour obtenir
+// le chemin absolu du fichier courant et de son dossier.
+// Cela permet de gérer correctement les chemins (ex: dossier /images) 
+// comme on le faisait en CommonJS.
 
 const app = express();
 
-mongoose.connect('mongodb+srv://nessrineferrah01_db_user:3lmG8p4biTTTeoCv@cluster0.qegigyn.mongodb.net/?appName=Cluster0',)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch((error) => {
   console.log('Connexion à MongoDB échouée !');
